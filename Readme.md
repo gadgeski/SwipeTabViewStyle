038c27ab7a8b40893c768a47bad122f02ef8e6b4
gadgeski <xtwh.ce5bil@gmail.com>
2025-04-18 22:43:33 +0900

    Xcodeでの動作確認も含め色々

---
+++ Readme.md
@@ -0,0 +1,5 @@
+・ファイルについて<br>
+ 1.ContentView<br>
+    別のツールで記述したコード<br>
+2.SwipeTabViewStyle<br>
+    Xcodeで1をビルドしたもの<br>
---
+++ SwipeTabViewStyle/SwipeTabViewStyle/SwipeTabViewStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeTabViewStyleApp.swift
+//  SwipeTabViewStyle
+//
+//  Created by Dev Tech on 2025/04/18.
+//
+
+import SwiftUI
+
+@main
+struct SwipeTabViewStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
