0b5e7ed583e80b9763da886b995ce2e04e7919e6
gadgeski <xtwh.ce5bil@gmail.com>
2025-04-18 15:39:50 +0900

    ContentView作成

---
+++ ContenView.swift
@@ -0,0 +1,78 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State var selectedTag = 1
+
+    var body: some View {
+        TabView(selection: $selectedTag) {
+            HomeTabView().tag(1)
+            WeatherTabView().tag(2)
+            NewsTabView().tag(3)
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .ignoresSafeArea()
+    }
+}
+
+struct HomeTabView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "house")
+            .resizable()
+            .frame(width: 100, height: 100)
+            Text("Home")
+            .font(.system(size: 20))
+        }
+    }
+}
+
+struct WeatherTabView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "cloud.sun")
+            .resizable()
+            .frame(width: 100, height: 100)
+            Text("お天気")
+            .font(.system(size: 20))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.mint.opacity(0.2))
+        .ignoresSafeArea()
+    }
+}
+
+struct NewsTabView: View{
+    var body: some View {
+        VStack {
+            Image(systemName: "newspaper")
+            .resizable()
+            .frame(width: 100, height: 100)
+            Text("ニュース")
+            .font(.system(size: 20))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct HomeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeTabView()
+    }
+}
+
+struct WeatherTbaView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherTabView()
+    }
+}
+
+struct NewsTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsTabView()
+    }
+}
